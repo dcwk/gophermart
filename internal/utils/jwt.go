6326442f7830e771fedcdbcb298fe4e5eadaf06d
Ruslan Golovizin <ruslan.golovizin@lamoda.ru>
2024-05-23 23:20:15 +0300

    Добавил функцию для расшифровку jwt токена

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -11,7 +12,10 @@ type Claims struct {
 	UserID int64
 }
 
+// TODO: добавить возможность управлять протуханием из конфига
 const TOKEN_EXP = time.Hour * 3
+
+// TODO: убрать ключ в конфиг
 const SECRET_KEY = "supersecretkey"
 
 func BuildJWTString(userID int64) (string, error) {
@@ -28,4 +32,27 @@ func BuildJWTString(userID int64) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+// TODO: Завернуть проверку в middleware
+func GetUserId(tokenString string) int64 {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(SECRET_KEY), nil
+		})
+	if err != nil {
+		return -1
+	}
+
+	if !token.Valid {
+		fmt.Println("Token is not valid")
+		return -1
+	}
+
+	fmt.Println("Token os valid")
+	return claims.UserID
 }
