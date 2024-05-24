96bd56f27cad3704350df868981420fadfa5a9d2
Ruslan Golovizin <ruslan.golovizin@lamoda.ru>
2024-05-24 23:53:46 +0300

    Добавил middleware для логирования запросов и подключил к роутеру

--- internal/application/register.go
+++ internal/application/register.go
@@ -11,15 +11,18 @@ func (app *Application) Register(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
+		app.Container.Logger().Info(err.Error())
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
 	newUser, err := app.Container.RegisterUserService().CreateUser(r.Context(), &user)
 	if err != nil {
+		app.Container.Logger().Info(err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 	if err := json.NewEncoder(w).Encode(newUser); err != nil {
+		app.Container.Logger().Info(err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
