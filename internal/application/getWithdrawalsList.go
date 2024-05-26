31421ba1218f636977fd4c70923cd604d7eaddc1
Ruslan Golovizin <ruslan.golovizin@lamoda.ru>
2024-05-26 22:46:58 +0300

    Добавил операцию получения списка выводов баллов

--- internal/application/getWithdrawalsList.go
+++ internal/application/getWithdrawalsList.go
@@ -1,9 +1,37 @@
 package application
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/dcwk/gophermart/internal/models"
+	"github.com/dcwk/gophermart/internal/utils/auth"
 )
 
-func (a *Application) GetWithdrawalsList(w http.ResponseWriter, r *http.Request) {
+type WithdrawalsListResponse struct {
+	WithdrawalsList []*models.Withdrawal `json:"withdrawals"`
+}
 
+func (app *Application) GetWithdrawalsList(w http.ResponseWriter, r *http.Request) {
+	userID := auth.GetUserIDFromCtx(r.Context())
+	withdrawals, err := app.Container.GetWithdrawalsService().Handle(
+		r.Context(),
+		userID,
+	)
+	if err != nil {
+		app.Container.Logger().Info(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp := WithdrawalsListResponse{WithdrawalsList: withdrawals}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		app.Container.Logger().Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	return
 }
