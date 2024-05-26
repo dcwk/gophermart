e9b18412d11ded50ac2b504e09ece090dc053c97
Ruslan Golovizin <ruslan.golovizin@lamoda.ru>
2024-05-26 22:50:44 +0300

    Исправил метод в репозитории

--- internal/repositories/withdrawalRepository.go
+++ internal/repositories/withdrawalRepository.go
@@ -22,22 +22,22 @@ func NewWithdrawalRepository(db *pgxpool.Pool) WithdrawalRepository {
 }
 
 func (r *withdrawalRepository) FindUserWithdrawals(ctx context.Context, userID int64) ([]*models.Withdrawal, error) {
-	rows, err := r.DB.Query(ctx, `SELECT id, order_id, value, created_at FROM withdrawal WHERE user_id=$1`, userID)
+	rows, err := r.DB.Query(ctx, `SELECT id, user_id, order_id, value, created_at FROM withdrawal WHERE user_id=$1`, userID)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var orders []*models.Order
+	var withdrawals []*models.Withdrawal
 	for rows.Next() {
-		order := models.Order{}
-		err := rows.Scan(&order.ID, &order.UserID, &order.Number, &order.CreatedAt)
+		withdrawal := models.Withdrawal{}
+		err := rows.Scan(&withdrawal.ID, &withdrawal.UserID, &withdrawal.OrderID, &withdrawal.Value)
 		if err != nil {
 			return nil, err
 		}
 
-		orders = append(orders, &order)
+		withdrawals = append(withdrawals, &withdrawal)
 	}
 
-	return orders, nil
+	return withdrawals, nil
 }
