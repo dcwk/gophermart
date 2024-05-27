c2bacfadc33409b101afa804bc7f6cf8fa4054dd
Ruslan Golovizin <ruslan.golovizin@lamoda.ru>
2024-05-27 18:30:02 +0300

    Добавил статусы Accrual и фабричный метод создания

--- internal/models/accrual.go
+++ internal/models/accrual.go
@@ -2,10 +2,25 @@ package models
 
 import "time"
 
+const (
+	NEW        = "NEW"
+	PROCESSING = "PROCESSING"
+	INVALID    = "INVALID"
+	PROCESSED  = "PROCESSED"
+)
+
 type Accrual struct {
 	OrderID   int64     `json:"order_id"`
 	Status    string    `json:"status"`
 	Value     float64   `json:"value"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewAccrual(orderId int64) *Accrual {
+	return &Accrual{
+		OrderID: orderId,
+		Status:  PROCESSING,
+		Value:   0,
+	}
 }
